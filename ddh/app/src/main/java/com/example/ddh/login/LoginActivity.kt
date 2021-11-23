3f26295361033d3234f92c9e1878b56c98bd3b26
onxmoreplz <hamdoyoung95@gmail.com>
2021-11-23 19:52:08 +0900

    [Feat] Change First activity to LoginActivity

---
+++ ddh/app/src/main/java/com/example/ddh/login/LoginActivity.kt
@@ -0,0 +1,25 @@
+package com.example.ddh.login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.ddh.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_Ddh)// 스플래시로 인해 변경되었던 Theme 되돌림
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        setBtn()
+    }
+
+    private fun setBtn() {
+        findViewById<Button>(R.id.btn_sign_up).setOnClickListener {
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
+        }
+    }
+}
