900740e88151570c514e47c7d474f632a6af3e5e
onxmoreplz <hamdoyoung95@gmail.com>
2021-11-23 19:51:17 +0900

    [Feat] Add Sign Up Activity

---
+++ ddh/app/src/main/java/com/example/ddh/login/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ddh.login
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ddh.R
+
+class SignUpActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
