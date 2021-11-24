0fdeb781f5d889c5e17b72e8cb6b32b25b6184b4
onxmoreplz <hamdoyoung95@gmail.com>
2021-11-24 20:49:37 +0900

    [Feat] Create SignUpPassword Activity

--- ddh/app/src/main/java/com/example/ddh/login/SignUpPasswordActivity.kt
+++ ddh/app/src/main/java/com/example/ddh/login/SignUpPasswordActivity.kt
@@ -1,9 +1,11 @@
 package com.example.ddh.login
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ddh.R
 
-class SignUpPasswordActivity : AppCompatActivity() {
+class SignUpPasswordActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_password)
