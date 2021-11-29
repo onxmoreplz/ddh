6f007742bca7c7a43c5423d64d153efb5a5107dc
onxmoreplz <hamdoyoung95@gmail.com>
2021-11-29 20:20:24 +0900

    [Chore] Create MainViewModel

--- ddh/app/src/main/java/com/example/ddh/main/MainViewModel.kt
+++ ddh/app/src/main/java/com/example/ddh/main/MainViewModel.kt
@@ -1,4 +1,13 @@
 package com.example.ddh.main
 
-class MainViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.ddh.data.repository.UserRepositoryImpl
+
+class MainViewModel(
+    private val userRepository: UserRepositoryImpl
+) {
+/*    private val _userInfoHashMap = MutableLiveData<HashMap<String, String>>()
+    val userInfoHashMap: LiveData<HashMap<String, String>> get() = _userInfoHashMap*/
+
 }
