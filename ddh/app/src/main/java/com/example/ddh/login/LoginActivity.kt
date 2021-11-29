f00dd7858753dbf191404ab66cf257277f08dc9b
onxmoreplz <hamdoyoung95@gmail.com>
2021-11-30 01:11:58 +0900

    [Update] Modify LoginActivity for using DataBinding

--- ddh/app/src/main/java/com/example/ddh/login/LoginActivity.kt
+++ ddh/app/src/main/java/com/example/ddh/login/LoginActivity.kt
@@ -1,18 +1,28 @@
 package com.example.ddh.login
 
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import com.example.ddh.R
+import com.example.ddh.data.repository.UserRepositoryImpl
+import com.example.ddh.databinding.ActivityLoginBinding
 import com.example.ddh.signup.SignUpEmailActivity
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : Activity() {
+
+    private lateinit var databinding: ActivityLoginBinding
+
+    private val userRepository = UserRepositoryImpl() // 의존성 주입을 위한 Repository 객체 생성
+    private val loginViewmodel = LoginViewModel(userRepository)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Ddh)// 스플래시로 인해 변경되었던 Theme 되돌림
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        databinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        databinding.vm = loginViewmodel
 
         setBtn()
     }
@@ -22,5 +32,6 @@ class LoginActivity : AppCompatActivity() {
         val intent = Intent(this, SignUpEmailActivity::class.java)
         startActivity(intent)
         }
+
     }
 }
