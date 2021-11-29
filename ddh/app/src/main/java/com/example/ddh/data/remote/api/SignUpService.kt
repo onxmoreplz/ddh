c2e2eb2bd7e26d7af7a0db5192e5652c9fab38d2
onxmoreplz <hamdoyoung95@gmail.com>
2021-11-30 00:26:42 +0900

    [Feat] Add SignUpService for using HTTP Method

---
+++ ddh/app/src/main/java/com/example/ddh/data/remote/api/SignUpService.kt
@@ -0,0 +1,16 @@
+package com.example.ddh.data.remote.api
+
+import com.example.ddh.data.dto.SignUpUserData
+import retrofit2.Call
+import retrofit2.http.*
+
+interface SignUpService {
+
+    @FormUrlEncoded // x-www-urlencoded로 보내겠다.
+    @POST("user/register")
+    fun postSignUp(
+        @FieldMap params: HashMap<String, String>
+    ): Call<SignUpUserData.SignUpUserResponse>
+
+
+}
