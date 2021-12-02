bd9490e180ee1158a3da4d6e02c9991f447b2674
onxmoreplz <hamdoyoung95@gmail.com>
2021-12-02 23:33:26 +0900

    [Feat] Make Button to come back LoginActivity

--- ddh/app/src/main/java/com/example/ddh/signup/SignUpCompleteActivity.kt
+++ ddh/app/src/main/java/com/example/ddh/signup/SignUpCompleteActivity.kt
@@ -1,10 +1,13 @@
 package com.example.ddh.signup
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ddh.R
+import com.example.ddh.login.LoginActivity
 
 
 class SignUpCompleteActivity : Activity() {
@@ -16,7 +19,10 @@ class SignUpCompleteActivity : Activity() {
         val userName = intent.getStringExtra("username")
 
         findViewById<TextView>(R.id.tv_user_name).text = "$userName 님 \nDDH의 회원이 되신 것을\n축하드립니다."
-
+        findViewById<Button>(R.id.btn_back_login).setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
