d1bf651e51ae6bdf314fbf7ba1b402babfe57aee
onxmoreplz <hamdoyoung95@gmail.com>
2021-12-08 22:20:20 +0900

    [Create] Create Sign Up Complete Activity
    - make handler to start Login Activity after 10seconds

--- ddh/app/src/main/java/com/example/ddh/signup/SignUpCompleteActivity.kt
+++ ddh/app/src/main/java/com/example/ddh/signup/SignUpCompleteActivity.kt
@@ -3,9 +3,9 @@ package com.example.ddh.signup
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import com.example.ddh.R
 import com.example.ddh.login.LoginActivity
 
@@ -15,14 +15,25 @@ class SignUpCompleteActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_complete)
 
+        setTvUserName()
+        setHandlerToBackLoginActivityAfter8seconds()
+
+    }
+
+    private fun setHandlerToBackLoginActivityAfter8seconds() {
+        Handler().postDelayed({
+            startActivity(Intent(this@SignUpCompleteActivity, LoginActivity::class.java))
+        }, 10000)
+    }
+
+    private fun setTvUserName() {
         val intent = intent
         val userName = intent.getStringExtra("username")
 
-        findViewById<TextView>(R.id.tv_user_name).text = "$userName 님 \nDDH의 회원이 되신 것을\n진심으로 환영합니다."
+        findViewById<TextView>(R.id.tv_user_name).text = "$userName 님 \n등산의 목적 가입이 완료되었습니다.\n지금부터 즐거운 등산을 시작해보세요!"
         findViewById<Button>(R.id.btn_back_login).setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
-
     }
 }
