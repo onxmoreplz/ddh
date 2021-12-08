eb15a173dc971164201a042b6a736b8575382a2c
onxmoreplz <hamdoyoung95@gmail.com>
2021-12-08 22:23:45 +0900

    [Feat] Make Fragment(home, search, ...) on MainActivity

--- ddh/app/src/main/java/com/example/ddh/main/MainActivity.kt
+++ ddh/app/src/main/java/com/example/ddh/main/MainActivity.kt
@@ -1,16 +1,68 @@
 package com.example.ddh.main
 
 import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
 import com.example.ddh.R
+import com.example.ddh.databinding.ActivityMainBinding
+import com.example.ddh.main.fragment.home.HomeFragment
+import com.example.ddh.main.fragment.search.SearchFragment
 
-class MainActivity : Activity() {
+class MainActivity : FragmentActivity() {
 
+    private lateinit var databinding: ActivityMainBinding
+
+    private val homeFragment by lazy { HomeFragment() }
+    private val searchFragment by lazy { SearchFragment() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        databinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        databinding.bnvHome.background = null
+        databinding.bnvHome.run {
+            setOnNavigationItemSelectedListener {
+                when (it.itemId) {
+                    R.id.home -> {
+                        setFragment(homeFragment)
+                    }
+                    R.id.feed -> {
+                        setFragment(searchFragment)
+                    }
+                    R.id.search -> {
+                        setFragment(searchFragment)
+                    }
+                    R.id.myPage -> {
+                        setFragment(searchFragment)
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.home // 초기 프래그먼트
+        }
+    }
+
+    /**
+     *  <p> 프래그먼트 전환 함수 </p>
+     *
+     * @param fragment: Fragment 전환될 프래그먼트
+     */
+    private fun setFragment(fragment: Fragment) {
+        val manager: FragmentManager = supportFragmentManager
+        manager.beginTransaction()
+            .replace(R.id.fl_container, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
