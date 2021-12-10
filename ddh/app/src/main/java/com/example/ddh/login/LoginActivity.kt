5caf09f69d3d0ad5a677a8423e0cf72398694ae5
onxmoreplz <hamdoyoung95@gmail.com>
2021-12-10 18:50:44 +0900

    [Feat] Add Kakao Social Login Button and Enroll callback listener

--- ddh/app/src/main/java/com/example/ddh/login/LoginActivity.kt
+++ ddh/app/src/main/java/com/example/ddh/login/LoginActivity.kt
@@ -2,20 +2,19 @@ package com.example.ddh.login
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.Log
-import android.widget.Button
 import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.ddh.R
 import com.example.ddh.data.repository.UserRepositoryImpl
 import com.example.ddh.databinding.ActivityLoginBinding
 import com.example.ddh.main.MainActivity
 import com.example.ddh.signup.SignUpEmailActivity
-import java.util.regex.Pattern
+import com.kakao.sdk.auth.LoginClient
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.AuthErrorCause
 
 class LoginActivity : Activity() {
 
@@ -38,37 +37,73 @@ class LoginActivity : Activity() {
         databinding.vm = loginViewmodel
 
 //        setObserveViewModelEvent()
+        setSocialLoginBtn()
         setBtn()
     }
 
-/*    private fun setObserveViewModelEvent() {
-        // 이메일 유효성 검사
-        loginViewmodel.emailChecking.addOnPropertyChangedCallback(object : Observable.OnPropertyChangedCallback() {
-            override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
-                databinding.etEmail.backgroundTintList = ContextCompat.getColorStateList(applicationContext, R.color.colorEditTextRed)
+    private fun setSocialLoginBtn() {
+        setKakaoLogin()
+    }
+
+    private fun setKakaoLogin() {
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+                Log.d("Kakao Login", error.toString())
+                when {
+                    error.toString() == AuthErrorCause.AccessDenied.toString() -> {
+                        Toast.makeText(this, "접근이 거부 됨(동의 취소)", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.InvalidClient.toString() -> {
+                        Toast.makeText(this, "유효하지 않은 앱", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.InvalidGrant.toString() -> {
+                        Toast.makeText(this, "인증 수단이 유효하지 않아 인증할 수 없는 상태", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.InvalidRequest.toString() -> {
+                        Toast.makeText(this, "요청 파라미터 오류", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.InvalidScope.toString() -> {
+                        Toast.makeText(this, "유효하지 않은 scope ID", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.Misconfigured.toString() -> {
+                        Toast.makeText(this, "설정이 올바르지 않음(android key hash)", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.ServerError.toString() -> {
+                        Toast.makeText(this, "서버 내부 에러", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.Unauthorized.toString() -> {
+                        Toast.makeText(this, "앱이 요청 권한이 없음", Toast.LENGTH_SHORT).show()
+                    }
+                    else -> { // Unknown
+                        Toast.makeText(this, "기타 에러", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else if (token != null) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+                finish()
             }
-        })
-        // 로그인 성공
-        loginViewmodel.successLogin.addOnPropertyChangedCallback(object : Observable.OnPropertyChangedCallback() {
-            override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
-                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+        }
+
+        // [카카오 로그인] 버튼 리스너
+        databinding.btnKakaoLogin.setOnClickListener {
+            if (LoginClient.instance.isKakaoTalkLoginAvailable(this)) {
+                LoginClient.instance.loginWithKakaoTalk(this, callback = callback)
+            } else {
+                Log.d("Kakao Login FAIL", "There is no KakaoTalk App in this phone")
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.kakao.talk&hl=ko&gl=US"))
                 startActivity(intent)
             }
-        })
-        // 로그인 실패
-        loginViewmodel.failLogin.addOnPropertyChangedCallback(object : Observable.OnPropertyChangedCallback() {
-            override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
-                Toast.makeText(this@LoginActivity, "---", Toast.LENGTH_SHORT).show()
-            }
-        })
-    }*/
+        }
 
+
+    }
 
     private fun setBtn() {
         // [로그인] 버튼
         databinding.btnLogin.setOnClickListener {
             if (databinding.etEmail.toString().isNotEmpty()) {
-                if(databinding.etPw.toString().isNotEmpty()) {
+                if (databinding.etPw.toString().isNotEmpty()) {
                     etEmail = databinding.etEmail.text.toString().trim()
                     etPassword = databinding.etPw.text.toString().trim()
                     postUserLogin(etEmail, etPassword)
