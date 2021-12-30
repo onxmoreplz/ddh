eca1fdff64742d2479615d7bce65934e2036039c
onxmoreplz <hamdoyoung95@gmail.com>
2021-12-31 01:21:46 +0900

    [Feat] Create Find Email, Password Activities

---
+++ ddh/app/src/main/java/com/example/ddh/login/find/FindIdActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ddh.login.find
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.ddh.R
+
+class FindIdActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_id)
+    }
+}
---
+++ ddh/app/src/main/java/com/example/ddh/login/find/FindResultIdActivity.kt
@@ -0,0 +1,14 @@
+package com.example.ddh.login.find
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.ddh.R
+
+class FindResultIdActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_result_id)
+
+
+    }
+}
