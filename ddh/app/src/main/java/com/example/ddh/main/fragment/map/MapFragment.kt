b186cf3aaff0fea9fc4d828c64bd3be623181685
onxmoreplz <hamdoyoung95@gmail.com>
2022-01-07 19:31:32 +0900

    [Feat] Add Map Fragment to get mountain by current location

---
+++ ddh/app/src/main/java/com/example/ddh/main/fragment/map/MapFragment.kt
@@ -0,0 +1,29 @@
+package com.example.ddh.upload
+
+import android.view.View
+import com.example.ddh.R
+import com.example.ddh.base.UtilityBase
+import com.example.ddh.databinding.FragmentMapBinding
+import com.example.ddh.databinding.FragmentSecondUploadBinding
+
+class MapFragment : UtilityBase.BaseFragment<FragmentMapBinding>(
+    R.layout.fragment_map
+), View.OnClickListener {
+
+    override fun FragmentMapBinding.onCreateView() {
+
+    }
+
+    override fun FragmentMapBinding.onViewCreated() {
+        setButton()
+    }
+
+    private fun setButton() {
+
+    }
+
+    override fun onClick(v: View?) {
+
+    }
+
+}
