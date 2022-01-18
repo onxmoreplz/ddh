3f34545eb03d36629b313211562a84939f8e9555
onxmoreplz <hamdoyoung95@gmail.com>
2022-01-18 20:56:51 +0900

    [Feat] Set Uplpad Activity

--- ddh/app/src/main/java/com/example/ddh/upload/UploadActivity.kt
+++ ddh/app/src/main/java/com/example/ddh/upload/UploadActivity.kt
@@ -1,7 +1,10 @@
 package com.example.ddh.upload
 
+import android.graphics.Color
+import android.opengl.Visibility
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ProgressBar
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -15,13 +18,16 @@ class UploadActivity : FragmentActivity(), View.OnClickListener {
 
     private lateinit var databinding: ActivityUploadBinding
     private var fragmentIndex = 0
+    lateinit var buttonNext: Button
 
     private val firstUploadFragment by lazy { FirstUploadFragment() }
     private val secondUploadFragment by lazy { SecondUploadFragment() }
     private val thirdUploadFragment by lazy { ThirdUploadFragment() }
+    private val previewFragment by lazy { PreviewFragment() }
 
     companion object {
         var progressBar = ProgressBar(App.instance) // 프로그레스 바
+        var uploadContentHashMap = HashMap<String, Any>()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,11 +42,18 @@ class UploadActivity : FragmentActivity(), View.OnClickListener {
         setButton()
     }
 
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+    }
+
     private fun setProgressBar() {
         progressBar = databinding.pbUpload
     }
 
     private fun setButton() {
+        buttonNext = databinding.btnNext
+
         databinding.btnBack.setOnClickListener(this)
         databinding.btnNext.setOnClickListener(this)
 
@@ -56,20 +69,32 @@ class UploadActivity : FragmentActivity(), View.OnClickListener {
                 } else if (fragmentIndex == 1) {
                     fragmentIndex = 0
                     replaceFragmentAddToBackStack(firstUploadFragment)
-                } else if(fragmentIndex == 2) {
+                } else if (fragmentIndex == 2) {
                     fragmentIndex = 1
                     replaceFragmentAddToBackStack(secondUploadFragment)
+                } else if (fragmentIndex == 3) {
+                    fragmentIndex = 2
+                    databinding.tvTitlePreview.visibility = View.GONE
+                    databinding.btnNext.visibility = View.VISIBLE
+                    replaceFragmentAddToBackStack(thirdUploadFragment)
                 }
             }
             databinding.btnNext -> {
                 if (fragmentIndex == 0) {
+                    firstUploadFragment.saveHashMapParty()
+
                     replaceFragmentAddToBackStack(secondUploadFragment)
                     fragmentIndex = 1
                 } else if (fragmentIndex == 1) {
+                    secondUploadFragment.saveHashMapParty()
                     replaceFragmentAddToBackStack(thirdUploadFragment)
                     fragmentIndex = 2
-                } else if(fragmentIndex == 2) {
-                    //
+                } else if (fragmentIndex == 2) {
+                    thirdUploadFragment.saveHashMapParty()
+                    databinding.tvTitlePreview.visibility = View.VISIBLE
+                    databinding.btnNext.visibility = View.GONE
+                    replaceFragmentAddToBackStack(previewFragment)
+                    fragmentIndex = 3
                 }
             }
         }
@@ -83,4 +108,16 @@ class UploadActivity : FragmentActivity(), View.OnClickListener {
             .addToBackStack(null)
             .commit()
     }
+
+    fun setNextButtonEnable() {
+        databinding.btnNext.isEnabled = false
+        databinding.btnNext.setTextColor(Color.rgb(207, 207, 207))
+    }
+
+    fun SetNextButtonAble() {
+        databinding.btnNext.isEnabled = true
+        databinding.btnNext.setTextColor(Color.rgb(115, 61, 203))
+    }
+
+
 }
